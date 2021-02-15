720754b88ad633df44b83f7189bde162cc1ab930
L0Angel98 <75176852+L0Angel98@users.noreply.github.com>
2021-02-15 17:59:01 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import "./styles.css";
+import React from "react";
+
+class SearchWord extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "", result: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const removeAccents = (str) => {
+      return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    };
+    var tex = this.state.text;
+    var word;
+    var chain = removeAccents(tex);
+    var texOriginal = chain.toLowerCase();
+    var letterSpace = texOriginal.split("");
+    var chainWithoutSpace = "";
+    for (i in letterSpace) {
+      if (letterSpace[i] !== " ") {
+        chainWithoutSpace += letterSpace[i];
+      }
+    }
+
+    var letters = chainWithoutSpace.split("");
+    var lettersReverse = chainWithoutSpace.split("").reverse();
+    var same = true;
+
+    for (i in letters) {
+      if (letters[i] === lettersReverse[i]) {
+      } else {
+        same = false;
+      }
+    }
+
+    if (same) {
+      word = " es un palíndromo";
+    } else {
+      word = " no es un palíndromo";
+    }
+    this.setState({ text: this.state.text, result: word });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <body>
+        <header className="hello">
+          <h1>Palabras palindromas</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              class="word"
+              id="text"
+              placeholder="Escribe la palabra"
+              value={this.state.text}
+              onChange={this.handleChange}
+            />
+            <input id="button" type="submit" value="Revisar" />
+          </form>
+        </header>
+
+        <section id="answer">
+          <h1 id="youWord">Tu palabra</h1>
+          <p id="result">{this.state.result}</p>
+        </section>
+
+        <footer>
+          <p>&copy; 2020 Luis Ángel Velázquez Palomino</p>
+        </footer>
+      </body>
+    );
+  }
+}
+
+export default function App() {
+  return <SearchWord />;
+}
