d462b917e6c1098f0cc9b0fcb7405da5bce35e9b
L0Angel98 <75176852+L0Angel98@users.noreply.github.com>
2021-02-18 10:17:04 -0600

    v1

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 
 //import App from "./App";
 //import Menu from "./Menu";
-//import Tablas from "./Tablas";
-import Formulario from "./Formulario";
+import Tablas from "./Tablas";
+//import Formulario from "./Formulario";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <Formulario />
+    <Tablas />
   </StrictMode>,
   rootElement
 );
