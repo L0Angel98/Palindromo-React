85c53e3ac51a10d3b79ab9c543acb79b87fbb983
L0Angel98 <75176852+L0Angel98@users.noreply.github.com>
2021-02-26 18:23:02 -0600

    Character

--- src/Layout/StarWars/Film/Film.js
+++ src/Layout/StarWars/Film/Film.js
@@ -1,38 +1,19 @@
 import React from "react";
+import DescriptionFilm from "./DescriptionFilm/DescriptionFilm";
 
-//import ListFilms from "./ListFilms/ListFilms"
-import CharacterName from "../Characters/CharactersName/CharactersName";
-class Film extends React.Component {
-  //constructor(props) {
-  // super(props);
-  //this.state = {
+const Film = ({ film, characterData }) => {
+  const ONE_FILM = film;
 
-  //};
-  //}
-  render() {
-    const ONE_FILM = this.props.film;
-    const CharacterList = ONE_FILM.characters.map((Character) => (
-      <li key={Character.id}>
-        <button>
-          <CharacterName ipName={Character} />
-        </button>
-      </li>
-    ));
-    return (
-      <div>
-        <h1>Title</h1>
-        <p>{ONE_FILM.title}</p>
-        <h1>Opening crawl</h1>
-        <p>{ONE_FILM.opening_crawl}</p>
-        <h1>Director</h1>
-        <p>{ONE_FILM.director}</p>
-        <h1>Producer</h1>
-        <p>{ONE_FILM.producer}</p>
-        <h1>Characters</h1>
-        <ul>{CharacterList}</ul>
-      </div>
-    );
-  }
-}
+  return (
+    <DescriptionFilm
+      title={ONE_FILM.title}
+      Opening={ONE_FILM.opening_crawl}
+      director={ONE_FILM.director}
+      producer={ONE_FILM.producer}
+      characters={ONE_FILM.characters}
+      characterData={characterData}
+    />
+  );
+};
 
 export default Film;
